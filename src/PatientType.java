4748acc3bed644bb61c7f79ae775f35cd0afa086
Gary Morris <gamorri1@asu.edu>
2016-10-12 14:43:00 -0700

    Prepped for version 6-12

--- src/PatientType.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Stanso on 3/8/2015.
- */
-public enum PatientType
-{
-    A,
-    B,
-}
--- src/ResourceType.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Stanso on 3/9/2015.
- */
-public enum ResourceType
-{
-    DOCTOR,
-    NURSE,
-    SURGEON,
-}
