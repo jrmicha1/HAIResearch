761aec35c17f409f5ec0fb6c08208ff71f35f088
cmbartee <cmbartee@asu.edu>
2017-03-28 19:42:10 -0700

    UID read in+saved; if no UID , "error" UID created
    UID is read in as the first argument passed in via String[] args.  If no
    argument is provided, a  64-char long string consisting of only capital
    letters is randomly generated to serve as the "error" UID.

--- Microworld_Hospital/src/MicroworldHospital.java
+++ Microworld_Hospital/src/MicroworldHospital.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
+import java.lang.StringBuffer;
 
 /**
  * Main Class of the Game Created by Stanso on 2/28/2015.
@@ -9,13 +11,30 @@ public class MicroworldHospital {
 
    static PrintWriter fileOut1, fileOut2;  //  PrintWriter for logging
    static MainFrame mainFrame;
+   private static String UID;
 
    /**
     * MAIN Entry of the program
     *
-    * @param args Unused
+    * @param args UID
     */
    public static void main(String[] args) {
+
+      try {
+         UID = args[0];
+      }
+      catch(ArrayIndexOutOfBoundsException e) {
+         //If no UID provided, generate "error" UID:
+         //"error" UID x2 length of UID (64vs32 chars)
+         //"error" UID only cap. letters, no spc or -
+         Random r = new Random();
+         StringBuffer s = new StringBuffer(64);
+         for(int i = 0; i < 64; i++){
+            s.append((char)(r.nextInt(26) + 'A'));
+         }
+         UID = s.toString();
+      }
+
       try {
          //System.out.println("Test");
          // set look and feel
@@ -143,5 +162,9 @@ public class MicroworldHospital {
       if (fileOut2 != null) {
          fileOut2.close();
       }
+   }
+
+   public static String getUID(){
+      return UID;
    }
 }
